3a61d4e995cd4fa2afa53eb69d91101199e6d2dd
Wallisson Martins <wallissonmartins37@gmail.com>
2022-08-05 23:51:51 -0300

    first commit

---
+++ src/config/dbConfig.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const dbConfig = process.env.DB_URL;
+
+const connection = mongoose.connect(dbConfig, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = connection;
---
+++ src/controllers/AnnotationController.js
@@ -0,0 +1,39 @@
+const Annotations = require('../models/AnnotationData');
+
+module.exports = {
+  async read(req, res) {
+    const annotationList = await Annotations.find();
+    return res.json(annotationList);
+  },
+  async create(req, res) {
+    const { title, notes, priority } = req.body;
+
+    if (!title || !notes) {
+      return res.json({ error: 'Necessáio um título/anotação!' });
+    }
+
+    const annotationCreated = await Annotations.create({
+      title,
+      notes,
+      priority,
+    });
+
+    return res.json(annotationCreated);
+  },
+  async delete(req, res) {
+    const { id } = req.params;
+
+    await Annotations.findByIdAndDelete(id)
+      .then(() => {
+        return res.json({
+          message: `${id} excluído com sucesso!`,
+        });
+      })
+      .catch(() => {
+        return res
+          .status(400)
+          .json({ error: 'Impossível deletar, registro não encontrado!' });
+      });
+    return res.status(500);
+  },
+};

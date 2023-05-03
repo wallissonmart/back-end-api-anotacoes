7945e355a737d7815fcd0b3d320633e43c926e1e
Wallisson Martins <wallissonmartins37@gmail.com>
2023-05-03 18:07:25 -0300

    format

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const dbConfig = process.env.DB_URL;
+const dbConfig = secrets.DB_URL;
 
 const connection = mongoose.connect(dbConfig, {
   useNewUrlParser: true,

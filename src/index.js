e7d375027343802e67f89b30cfdcde2edb7506f0
Wallisson Martins <wallissonmartins37@gmail.com>
2023-05-04 21:39:51 -0300

    ci/cd

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const PORT = process.env.PORT || 3010;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta: http://localhost:${PORT}...`);
